36abb858189f2f17adfb0e152a6172fade127d8f
Luciano Brito <lucianobrito.dev@gmail.com>
2021-05-14 21:41:29 -0300

    Created barrels

---
+++ src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './aluno.repository';
+export * from './curso.repository';
+export * from './professor.repository';
+export * from './usuario.repository';
+export * from './repository';
---
+++ src/routes/index.ts
@@ -0,0 +1,2 @@
+export * from './auth.router';
+export * from './cadastro.router';
