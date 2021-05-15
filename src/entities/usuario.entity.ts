a9c93dde785d95c75d653de07b5b73d5537fac25
Luciano Brito <lucianobrito.dev@gmail.com>
2021-05-14 21:50:10 -0300

    Update usuario.entity.ts

--- src/entities/usuario.entity.ts
+++ src/entities/usuario.entity.ts
@@ -4,7 +4,7 @@ export default class Usuario extends Entity {
   email: string;
   senha: string;
   nome: string;
-
+  curso: string;
   tipo: number; // 1 - Professor    2 - Aluno
 
   constructor() {
