621042e32b19ba320cef723ac483881709c535de
Luciano Brito <lucianobrito.dev@gmail.com>
2021-05-14 21:37:10 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
-import logger from 'morgan';
-import AlunoRouter from './routes/aluno.router';
-import AulaRouter from './routes/aula.router';
-import AuthRouter from './routes/auth.router';
-import CursoRouter from './routes/curso.router';
-import ProfessorRouter from './routes/professor.router';
-import Database from './utils/database/database';
-import Exception from './utils/exceptions/exception';
-import auth from './utils/middlewares/auth.middleware';
+import cors from "cors";
+import express, { NextFunction, Request, Response, Router } from "express";
+import logger from "morgan";
+import CadastroRouter from "./routes/cadastro.router";
+import AlunoRouter from "./routes/aluno.router";
+import AulaRouter from "./routes/aula.router";
+import AuthRouter from "./routes/auth.router";
+import CursoRouter from "./routes/curso.router";
+import ProfessorRouter from "./routes/professor.router";
+import Database from "./utils/database/database";
+import Exception from "./utils/exceptions/exception";
+import auth from "./utils/middlewares/auth.middleware";
 
+const router = express.Router();
 class App {
   public app: express.Application;
 
@@ -23,45 +25,57 @@ class App {
   }
 
   private database() {
-    Database.connect().then((r) => console.log('[Database] - Conexão realizada com sucesso'));
+    Database.connect().then((r) =>
+      console.log("[Database] - Conexão realizada com sucesso")
+    );
   }
 
   private initMiddlewares() {
     this.app.use(cors());
     this.app.use(express.json());
-    this.app.use(logger('dev'));
+    this.app.use(logger("dev"));
   }
 
-  private endMiddlewares() {
-    this.app.use((err: Exception | Error, req: Request, res: Response, next: NextFunction) => {
-      if (err) {
-        if (err instanceof Exception) {
-          res.status(err.status).json({ message: err.message, status: err.status });
-        } else {
-          // evite utilizar o Error diretamente. De preferencia para as Exceptions
-          res.status(400).json({ message: err.message, status: 400 });
-        }
-      }
-      next();
-    });
-  }
-
-  // #pegabandeira
+  // #pegabandeira resolvido
   private publicRoutes() {
-    this.app.use('/stefanini', AuthRouter);
+    this.app.use("/stefanini", CadastroRouter);
+    this.app.use("/stefanini", AuthRouter);
   }
 
   // #pegabandeira
   private privateRoutes() {
     this.authMiddleware();
-    this.app.use('/stefanini', AlunoRouter);
-    this.app.use('/stefanini', AulaRouter);
-    this.app.use('/stefanini', CursoRouter);
-    this.app.use('/stefanini', ProfessorRouter);
+    this.app.use("/stefanini", AlunoRouter);
+    this.app.use("/stefanini", AulaRouter);
+    this.app.use("/stefanini", CursoRouter);
+    this.app.use("/stefanini", ProfessorRouter);
   }
 
   private authMiddleware() {
     this.app.use(auth);
+  }
+
+  private endMiddlewares() {
+    this.app.use(
+      (
+        err: Exception | Error,
+        req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        if (err) {
+          if (err instanceof Exception) {
+            res
+              .status(err.status)
+              .json({ message: err.message, status: err.status });
+          } else {
+            // evite utilizar o Error diretamente. De preferencia para as Exceptions
+            res.status(400).json({ message: err.message, status: 400 });
+          }
+        }
+        next();
+      }
+    );
   }
 }
 
